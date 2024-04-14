79cc01d16bc51918d9d6d12fb24e487287a1597c
Marc Soda <marc@soda.fm>
2024-04-13 22:01:25 -0400

    defer counsel better

--- init.el
+++ init.el
@@ -108,7 +108,8 @@
 (use-package counsel
   :ensure
   :defer t
-  :bind ("M-x" . 'counsel-M-x)
+  :bind (("M-x" . 'counsel-M-x)
+         ("C-x C-f" . 'counsel-find-file))
   :config
   (counsel-mode t))
 
