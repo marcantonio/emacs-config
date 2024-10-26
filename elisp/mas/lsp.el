262b7e46bd909c16082cd56deb17a6596693d516
Marc Soda <marc@soda.fm>
2024-10-26 14:27:55 -0400

    lsp booster

--- elisp/mas/lsp.el
+++ elisp/mas/lsp.el
@@ -19,7 +19,7 @@
               ("C-c C-c C-j" . flycheck-next-error)
               ("C-c C-c C-k" . flycheck-previous-error))
   :custom
-  (lsp-idle-delay 0.1)
+  (lsp-idle-delay 0.5)
   (lsp-signature-render-documentation nil)
   (lsp-lens-enable nil)
   :config
@@ -186,5 +186,36 @@
   :load-path "~/.emacs.d/elisp/vendor/treesit-fold"
   :config
   (global-treesit-fold-indicators-mode))
+
+(defun lsp-booster--advice-json-parse (old-fn &rest args)
+  "Try to parse bytecode instead of json."
+  (or
+   (when (equal (following-char) ?#)
+     (let ((bytecode (read (current-buffer))))
+       (when (byte-code-function-p bytecode)
+         (funcall bytecode))))
+   (apply old-fn args)))
+(advice-add (if (progn (require 'json)
+                       (fboundp 'json-parse-buffer))
+                'json-parse-buffer
+              'json-read)
+            :around
+            #'lsp-booster--advice-json-parse)
+
+(defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+  "Prepend emacs-lsp-booster command to lsp CMD."
+  (let ((orig-result (funcall old-fn cmd test?)))
+    (if (and (not test?)                             ;; for check lsp-server-present?
+             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+             lsp-use-plists
+             (not (functionp 'json-rpc-connection))  ;; native json-rpc
+             (executable-find "emacs-lsp-booster"))
+        (progn
+          (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+            (setcar orig-result command-from-exec-path))
+          (message "Using emacs-lsp-booster for %s!" orig-result)
+          (cons "emacs-lsp-booster" orig-result))
+      orig-result)))
+(advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
 
 (provide 'mas/lsp)
