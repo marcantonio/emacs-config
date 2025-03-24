81a251657142132cda540958ed0b5afb4070cdca
Marc Soda <marc@soda.fm>
2025-03-24 12:31:07 -0400

    pyright

--- lisp/mas/python.el
+++ lisp/mas/python.el
@@ -1,6 +1,10 @@
 (require 'mas/dev)
 (require 'mas/lsp)
 
+
+;; https://blog.serghei.pl/posts/emacs-python-ide/
+(use-package lsp-pyright :ensure)
+
 ;;(require 'tramp)
 
 ;; this makes everything work...
@@ -9,16 +13,16 @@
 ;; (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
 ;; (add-to-list 'tramp-remote-path "/home/mas/.pyenv/versions/index/bin")
 ;; (setq lsp-pylsp-server-command "pylsp")
-;; (setq lsp-pylsp-plugins-pydocstyle-ignore ["D100" "D101" "D102" "D103" "D107"])
+;; (setq lsp-pylsp-plugins-pydocstyle-ignore ["D100" "D101" "D102" "D103" "D107" "D401"])
 ;; (setq lsp-pylsp-plugins-flake8-ignore ["E302" "E501"])
 
 ;; pip install "python-lsp-server[all]"
-(add-hook 'lsp-managed-mode-hook
-          (lambda ()
-            (flycheck-disable-checker 'python-pylint)
-            ;;(flycheck-select-checker 'python-flake8)))
-            (flycheck-disable-checker 'python-flake8)))
-(setq flycheck-flake8rc "~/.emacs.d/flake8.cfg")
+;; (add-hook 'lsp-managed-mode-hook
+;;           (lambda ()
+;;             (flycheck-disable-checker 'python-pylint)
+;;             ;;(flycheck-select-checker 'python-flake8)))
+;;             (flycheck-disable-checker 'python-flake8)))
+;;(setq flycheck-flake8rc "~/.emacs.d/flake8.cfg")
 (mas/load-dev-mode 'python-ts-mode)
 
 (provide 'mas/python)
