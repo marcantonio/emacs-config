30e534a9256c76a63cc337855afc1ec0b2f798d1
Marc Soda <marc@soda.fm>
2025-04-11 19:13:34 -0400

    python tweaks

--- lisp/mas/python.el
+++ lisp/mas/python.el
@@ -9,23 +9,10 @@
   :ensure
   :config
   (setq lsp-pyright-langserver-command "basedpyright")
-  (setq lsp-pyright-venv-path "/opt/pyenv/versions/3.12.7")
-  (setq lsp-pyright-extra-paths ["/usr/local/ddg-index/lib"])
+  (setq lsp-pyright-venv-path "")
+  (setq lsp-pyright-extra-paths [])
   (setq lsp-pyright-type-checking-mode "off"))
 
-;;(require 'tramp)
-;; this makes everything work...
-;; (setq lsp-log-io t)
-;; (setq tramp-default-method "ssh")
-;; (add-to-list 'tramp-remote-path 'tramp-own-remote-path)
-;; (add-to-list 'tramp-remote-path "/home/mas/.pyenv/versions/index/bin")
-
-;; pip install "python-lsp-server[all]"
-;; (add-hook 'lsp-managed-mode-hook
-;;           (lambda ()
-;;             (flycheck-disable-checker 'python-pylint)
-;;             ;;(flycheck-select-checker 'python-flake8)))
-;;             (flycheck-disable-checker 'python-flake8)))
 ;; (setq flycheck-flake8rc "~/.emacs.d/flake8.cfg")
 
 (mas/load-dev-mode 'python-ts-mode)
